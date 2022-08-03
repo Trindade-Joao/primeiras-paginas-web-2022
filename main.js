a673bd358e714babf660e6611e261ac56473f3bc
Trindade-Joao <joao.a.trindade.jr@gmail.com>
2022-08-03 08:48:52 -0300

    pagina principal finalizada iniciado paginas de conteudo

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const listaBotoesFavoritos = document.querySelectorAll('.favorito')
+const listaClassificacao = document.querySelectorAll('[data-classificacao]');
+const listaConteudo = document.querySelectorAll('[data-conteudo]')
+
+
 
 
 for (let contador = 0; contador < listaBotoesFavoritos.length; contador++){
@@ -13,6 +17,42 @@ for (let contador = 0; contador < listaBotoesFavoritos.length; contador++){
     }
 }
 
+desativaConteudos = () => {
+    listaConteudo.forEach(conteudo => conteudo.classList.add('hide'));
+}
+
+limpaBarraClassificacao = () => {
+    listaClassificacao.forEach(classificacao => classificacao.classList.remove('ativa'));
+}
+
+selecionaItemBarraClassificacao = (classificacao) => {
+    classificacao.classList.add('ativa')
+}
+
+selecionaConteudoMostrado = (valor) => {
+    
+
+    if(valor === 'todos') {
+        listaConteudo.forEach(conteudo => conteudo.classList.remove('hide'))
+    }else{
+        for (let contador = 0; contador < listaConteudo.length; contador++){
+            const valorConteudo = listaConteudo[contador].dataset.conteudo
+            if(valor === valorConteudo){
+                listaConteudo[contador].classList.remove('hide');
+            }
+        }        
+    }
+}
 
 
 
+listaClassificacao.forEach(classificacao => classificacao.addEventListener('click', () => {
+    const valor = classificacao.dataset.classificacao;
+
+    console.log(valor);
+    desativaConteudos();
+    limpaBarraClassificacao();
+    selecionaItemBarraClassificacao(classificacao);
+    selecionaConteudoMostrado(valor);
+
+}))
